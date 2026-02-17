e6f2de86750ee8d44c8ed94d0277345bd74ccf97
Sofia-Ashlie <b2029saqpadilla@pshs.edu.ph>
2026-02-17 22:41:08 +0800

    multiple polls

--- public/javascript/poll.js
+++ public/javascript/poll.js
@@ -1,36 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const pollForm = 
-        document.getElementById("poll-form");
-    const yesCount = 
-        document.getElementById("yes-count");
-    const noCount = 
-        document.getElementById("no-count");
-    let yesVotes = parseInt(localStorage.getItem("yesVotes")) || 0;
-    let noVotes =  parseInt(localStorage.getItem("noVotes")) ||0;
-
-    updateResults()
     
-    pollForm.addEventListener("submit", function (e) {
+    function initPoll(formId, yesCountId, noCountId, storage){
+        const pollForm = 
+            document.getElementById(formId);
+        const yesCount = 
+            document.getElementById(yesCountId);
+        const noCount = 
+            document.getElementById(noCountId);
+        let yesVotes = parseInt(localStorage.getItem(storage+"_yes")) || 0;
+        let noVotes =  parseInt(localStorage.getItem(storage+"_no")) ||0;
 
-        // It will help to prevent the submission of 
-        // form, so that following code can execute
-        e.preventDefault();
-        const formData = new FormData(pollForm);
-        const userVote = formData.get("vote");
-
-        if (userVote === "yes") {
-            yesVotes++;
-             localStorage.setItem("yesVotes", yesVotes);
-
-        } else if (userVote === "no") {
-            noVotes++;
-             localStorage.setItem("noVotes", noVotes);
-        }
-        updateResults();
-    });
-
-    function updateResults() {
+    const updateResults = () => {
         yesCount.textContent = yesVotes;
         noCount.textContent = noVotes;
+    };
+    updateResults();
+
+    pollForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+       
+        const formData = new FormData(pollForm);
+            const userVote = formData.get("vote");
+
+            if (userVote === "yes") {
+                yesVotes++;
+                localStorage.setItem(storage + "_yes", yesVotes);
+            } else if (userVote === "no") {
+                noVotes++;
+                localStorage.setItem(storage + "_no", noVotes);
+            }
+            updateResults();
+        });
     }
+
+            initPoll("poll-form", "yes-count", "no-count", "poll-question");
+
+            initPoll("poll-form2", "yes-count2", "no-count2", "poll-question2");
+
+            
+
 });
