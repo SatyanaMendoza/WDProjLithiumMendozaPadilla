83ea9c3d66f436fcd4b7fcf6b66c0db6a05673de
Sofia-Ashlie <b2029saqpadilla@pshs.edu.ph>
2026-02-17 22:50:27 +0800

    poll js comments

--- public/javascript/poll.js
+++ public/javascript/poll.js
@@ -1,42 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
     
-    function initPoll(formId, yesCountId, noCountId, storage){
-        const pollForm = 
-            document.getElementById(formId);
-        const yesCount = 
-            document.getElementById(yesCountId);
-        const noCount = 
-            document.getElementById(noCountId);
-        let yesVotes = parseInt(localStorage.getItem(storage+"_yes")) || 0;
-        let noVotes =  parseInt(localStorage.getItem(storage+"_no")) ||0;
+    function initPoll(formId, yesCountId, noCountId, storage)
+    {
+        const pollForm = document.getElementById(formId);
+        const yesCount = document.getElementById(yesCountId);
+        const noCount =   document.getElementById(noCountId);
 
-    const updateResults = () => {
-        yesCount.textContent = yesVotes;
-        noCount.textContent = noVotes;
-    };
-    updateResults();
+        let yesVotes = parseInt(localStorage.getItem(storage + "_yes")) || 0;
+        let noVotes =  parseInt(localStorage.getItem(storage + "_no")) || 0;
 
-    pollForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-       
-        const formData = new FormData(pollForm);
+        const updateResults = () => {
+            yesCount.textContent = yesVotes;
+            noCount.textContent = noVotes;
+        };
+        updateResults();
+
+        pollForm.addEventListener("submit", function (e) {
+
+            e.preventDefault();
+            const formData = new FormData(pollForm);
             const userVote = formData.get("vote");
 
-            if (userVote === "yes") {
-                yesVotes++;
-                localStorage.setItem(storage + "_yes", yesVotes);
-            } else if (userVote === "no") {
-                noVotes++;
-                localStorage.setItem(storage + "_no", noVotes);
-            }
-            updateResults();
-        });
+            //Count votes and store them into local storage
+                if (userVote === "yes") {
+                    yesVotes++;
+                    localStorage.setItem(storage + "_yes", yesVotes);
+                } else if (userVote === "no") {
+                    noVotes++;
+                    localStorage.setItem(storage + "_no", noVotes);
+                }
+                updateResults();
+            });
     }
-
+        //Call the function
             initPoll("poll-form", "yes-count", "no-count", "poll-question");
 
             initPoll("poll-form2", "yes-count2", "no-count2", "poll-question2");
 
             
-
 });
