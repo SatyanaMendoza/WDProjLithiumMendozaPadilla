dd1b7d9f8b66d6efda59591cc0ca7b90ff177edc
Sofia-Ashlie <b2029saqpadilla@pshs.edu.ph>
2026-02-18 20:21:32 +0800

    10 polls

--- public/javascript/poll.js
+++ public/javascript/poll.js
@@ -50,6 +50,14 @@ document.addEventListener("DOMContentLoaded", function () {
         //Call the function
             initPoll("poll-form", "one-count", "two-count", "three-count", "four-count", "poll-question");
             initPoll("poll-form2", "one-count2", "two-count2", "three-count2", "four-count2", "poll-question2");
+            initPoll("poll-form3", "one-count3", "two-count3", "three-count3", "four-count3", "poll-question3");
+            initPoll("poll-form4", "one-count4", "two-count4", "three-count4", "four-count4", "poll-question4");
+            initPoll("poll-form5", "one-count5", "two-count5", "three-count5", "four-count5", "poll-question5");
+            initPoll("poll-form6", "one-count6", "two-count6", "three-count6", "four-count6", "poll-question6");
+            initPoll("poll-form7", "one-count7", "two-count7", "three-count7", "four-count7", "poll-question7");
+            initPoll("poll-form8", "one-count8", "two-count8", "three-count8", "four-count8", "poll-question8");
+            initPoll("poll-form9", "one-count9", "two-count9", "three-count9", "four-count9", "poll-question9");
+            initPoll("poll-form10", "one-count10", "two-count10", "three-count10", "four-count10", "poll-question10");
 
             
 });
