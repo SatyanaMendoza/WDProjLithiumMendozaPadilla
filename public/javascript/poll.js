e816cae3af792074cb8bdbc842653f05cec9a730
Sofia-Ashlie <b2029saqpadilla@pshs.edu.ph>
2026-02-21 23:31:18 +0800

    poll done

--- public/javascript/poll.js
+++ public/javascript/poll.js
@@ -5,15 +5,24 @@ function toggleMenu() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Sofia if ur seeing this basicaly you have to chagne the id number (to the poll number) and in html you just change
-    //  the form id "poll-form(then number)"" you make sure to change the number there also if youre 
-    // gonna work on that my bad i changed the js to make it a little better for the thingy </3
+ 
     const polls = [
         { id: "1", opts: { one: "Spring", two: "Summer", three: "Fall", four: "Winter" } },
         { id: "2", opts: { one: "Festival of Ice: Witness festive ice sculptures and igloos, and join the Ice Fishing Contest!", 
             two: "SquidFest: Catch squid on the beach and win bountiful prizes!", 
             three: "Night Market: Go shopping, watch a mermaid show, and catch unique fish!", 
             four: "Feast of the Winter Star: Participate in a secret gift-giving event with the other villagers in Pelican Town!" } },
+        { id: "3", opts: { one: "Your Farm", two: "Pelican Town", three: "The Beach", four: "The Mountains" } },
+        { id: "4", opts: { one: "Dog", two: "Cat", three: "Turtle", four: "Horse" } },
+        { id: "5", opts: { one: "Foraging", two: "Farming", three: "Mining", four: "Fishing" } },
+        { id: "6", opts: { one: "Pigs", two: "Cows", three: "Goats", four: "Sheep" } },
+        { id: "7", opts: { one: "Alex", two: "Sam", three: "Haley", four: "Abigail" } },
+        { id: "8", opts: { one: "A whole cheese pizza recipe", two: "A recipe to make fluffy Pancakes", three: "A cookie recipe", four: "A recipe to make maki rolls" } },
+        { id: "9", opts: { one: "Attend aerobics class with Caroline and her friends!", 
+            two: "Help upgrade your most used tools with Clint at the Blacksmith!", 
+            three: "Bake cookies with Evelyn!", 
+            four: "Manage the Archaeology Museum with Gunther!" } },
+        { id: "10", opts: { one: "Cook yourself a fresh meal", two: "Go to bed", three: "Watch TV", four: "Decorate your room" } },
     ];
 
     polls.forEach(poll => {
