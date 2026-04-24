a2536965e605360af3ae4891eadb3141428b3b3a
Sofia-Ashlie <b2029saqpadilla@pshs.edu.ph>
2026-04-25 01:56:36 +0800

    hover

--- public/javascript/menu.js
+++ public/javascript/menu.js
@@ -4,14 +4,3 @@ function toggleMenu() {
   menu.classList.toggle('active');
 }
 
-//
-const scrollArrow = document.getElementById('scrollArrow');
-const hideAfter = 200; // pixels
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > hideAfter) {
-    scrollArrow.classList.add('hidden');
-  } else {
-    scrollArrow.classList.remove('hidden');
-  }
-});
